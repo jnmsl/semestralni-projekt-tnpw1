67126814c7acf4611a35b6de47956ec659a28ed1
jnmsl <honza.mu@gmail.com>
2021-12-01 01:11:27 +0100

    navbar & header

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Semestrální projekt Statické webové stránky
- 
---
+++ src/js/navbar.js
@@ -0,0 +1,5 @@
+function showMenu() {
+  document.querySelector('.nav-links').classList.toggle('nav-active');
+  document.querySelector('.nav-open').classList.toggle('hide');
+  document.querySelector('.nav-close').classList.toggle('show');
+}
