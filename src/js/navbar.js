05090c281b7a655231ff1ba75308c327bd5d8991
Jan Musil <janmusil@Jan-MacBook-Pro.local>
2021-12-02 02:58:25 +0100

    gh-pages

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,4 @@
-function showMenu() {
+export function showMenu() {
   document.querySelector('.nav-links').classList.toggle('nav-active');
   document.querySelector('.nav-open').classList.toggle('hide');
   document.querySelector('.nav-close').classList.toggle('show');
---
+++ src/js/scripts.js
@@ -0,0 +1,3 @@
+import { showMenu } from "./navbar.js";
+
+document.querySelector('a.nav-btn-toggle').addEventListener('click', showMenu)
