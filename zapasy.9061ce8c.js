67dcfd200a67dacdb74d77f0e5ecb7b9a814da0c
jnmsl <honza.mu@gmail.com>
2021-12-02 17:47:59 +0100

    Updates

--- zapasy.9061ce8c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("a.nav-btn-toggle").addEventListener("click",(function(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}));
-//# sourceMappingURL=zapasy.9061ce8c.js.map
--- zapasy.90c79859.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("a.nav-btn-toggle").addEventListener("click",(function(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}));
-//# sourceMappingURL=zapasy.90c79859.js.map
