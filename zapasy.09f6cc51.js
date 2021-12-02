37a788a5b4f14d550559de7740b723a2fc7c8d0e
jnmsl <honza.mu@gmail.com>
2021-12-02 20:27:30 +0100

    Updates

--- zapasy.09f6cc51.js
+++
@@ -1,2 +0,0 @@
-function showMenu(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}
-//# sourceMappingURL=zapasy.09f6cc51.js.map
