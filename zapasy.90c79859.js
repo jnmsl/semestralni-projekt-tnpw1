4799e09f638a9f31c4194fcdae4ec824b9cf04ee
Jan Musil <janmusil@Jan-MacBook-Pro.local>
2021-12-02 03:11:33 +0100

    Updates

---
+++ zapasy.90c79859.js
@@ -0,0 +1,2 @@
+document.querySelector("a.nav-btn-toggle").addEventListener("click",(function(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}));
+//# sourceMappingURL=zapasy.90c79859.js.map
