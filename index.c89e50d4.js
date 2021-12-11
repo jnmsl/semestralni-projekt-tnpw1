3b658833d04f276fd2a67b56b4be3778c90afaa8
jnmsl <honza.mu@gmail.com>
2021-12-11 10:41:35 +0100

    Updates

---
+++ index.c89e50d4.js
@@ -0,0 +1,2 @@
+document.querySelector("a.nav-btn-toggle").addEventListener("click",(function(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}));
+//# sourceMappingURL=index.c89e50d4.js.map
