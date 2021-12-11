f2b03b127bd2c5ea851363a3f03d6721c1d2e805
jnmsl <honza.mu@gmail.com>
2021-12-11 10:17:39 +0100

    edit scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,7 @@
-export function showMenu() {
-  document.querySelector('.nav-links').classList.toggle('nav-active');
-  document.querySelector('.nav-open').classList.toggle('hide');
-  document.querySelector('.nav-close').classList.toggle('show');
-}
-
-document.querySelector('a.nav-btn-toggle').addEventListener('click', showMenu);
+document
+  .querySelector('a.nav-btn-toggle')
+  .addEventListener('click', function () {
+    document.querySelector('.nav-links').classList.toggle('nav-active'),
+      document.querySelector('.nav-open').classList.toggle('hide'),
+      document.querySelector('.nav-close').classList.toggle('show');
+  });
