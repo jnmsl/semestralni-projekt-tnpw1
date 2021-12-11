5958618059ef05ead6178dc64d5ff5a9f8f072f6
jnmsl <honza.mu@gmail.com>
2021-12-11 09:45:40 +0100

    edit scripts

--- src/js/navbar.js
+++
@@ -1,5 +0,0 @@
-export function showMenu() {
-  document.querySelector('.nav-links').classList.toggle('nav-active');
-  document.querySelector('.nav-open').classList.toggle('hide');
-  document.querySelector('.nav-close').classList.toggle('show');
-}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,7 @@
-import { showMenu } from "./navbar.js";
+export function showMenu() {
+  document.querySelector('.nav-links').classList.toggle('nav-active');
+  document.querySelector('.nav-open').classList.toggle('hide');
+  document.querySelector('.nav-close').classList.toggle('show');
+}
 
-document.querySelector('a.nav-btn-toggle').addEventListener('click', showMenu)
+document.querySelector('a.nav-btn-toggle').addEventListener('click', showMenu);
