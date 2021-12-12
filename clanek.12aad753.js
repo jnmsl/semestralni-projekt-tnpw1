9ffa5e63379f42bf502749d6757760c9ebd7eeec
jnmsl <honza.mu@gmail.com>
2021-12-12 12:26:10 +0100

    Updates

---
+++ clanek.12aad753.js
@@ -0,0 +1,2 @@
+document.querySelector("a.nav-btn-toggle").addEventListener("click",(function(){document.querySelector(".nav-links").classList.toggle("nav-active"),document.querySelector(".nav-open").classList.toggle("hide"),document.querySelector(".nav-close").classList.toggle("show")}));
+//# sourceMappingURL=clanek.12aad753.js.map
