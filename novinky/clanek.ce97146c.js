4f4a797e693a2f7855afb3eb3f4fa7b40914ca97
jnmsl <honza.mu@gmail.com>
2021-12-12 12:21:05 +0100

    Updates

--- novinky/clanek.ce97146c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".social-share").innerHTML='<a href="https://twitter.com/intent/tweet">\n<img\n  src="../img/twitter.svg"\n  width="25"\n  height="25"\n  alt="Sdílet na twitteru"\n/>\n</a>';
-//# sourceMappingURL=clanek.ce97146c.js.map
